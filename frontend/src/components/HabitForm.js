0b338a1cbf2a6188a3fad59ead0267cf5b374d34
Rajnish Kumar <oshorajnishraj@gmail.com>
2024-02-18 17:45:37 +0530

    used useReducer and media query

--- frontend/src/components/HabitForm.js
+++ frontend/src/components/HabitForm.js
@@ -1,15 +1,36 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import '../styles/HabitForm.css';
 
+const formReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_TITLE':
+            return { ...state, title: action.payload };
+        case 'SET_REPS':
+            return { ...state, reps: action.payload };
+        case 'SET_ERROR':
+            return { ...state, error: action.payload };
+        case 'SET_SUCCESS':
+            return { ...state, success: action.payload };
+        default:
+    }
+}
+
+const initalState = {
+    title: '',
+    reps: '',
+    error: null,
+    success: null
+};
+
 const HabitForm = () => {
-    const [title, setTitle] = useState('');
-    const [reps, setReps] = useState('');
-    const [error, setError] = useState(null);
-    const [success, setSuccess] = useState(null);
+    const [state, dispatch] = useReducer(formReducer, initalState);
 
     const submitHandler = async (e) => {
         e.preventDefault();
-        const newHabit = { title, reps };
+        const newHabit = {
+            title: state.title,
+            reps: state.reps
+        };
         const response = await fetch('http://localhost:3400/api/todos', {
             method: 'POST',
             headers: {
@@ -20,33 +41,42 @@ const HabitForm = () => {
         const json = await response.json();
 
         if (!response.ok) {
-            setSuccess(null);
-            setError(json.message);
+            dispatch({ type: 'SET_SUCCESS', payload: null })
+            dispatch({ type: 'SET_ERROR', payload: json.message });
         }
         else {
-            setTitle('');
-            setReps('');
-            setError(null);
-            setSuccess('New habit added!');
+            dispatch({ type: 'SET_TITLE', payload: '' })
+            dispatch({ type: 'SET_REPS', payload: '' })
+            dispatch({ type: 'SET_ERROR', payload: null })
+            dispatch({ type: 'SET_SUCCESS', payload: 'New habit added!' });
         }
+    }
+
+    const changeHandler = (e) => {
+        const { name, value } = e.target;
+        dispatch({ type: `SET_${name.toUpperCase()}`, payload: value })
     }
 
     return (
         <form className='form' onSubmit={submitHandler}>
             <h3>Add a new habit</h3>
             <label>Habit:</label>
-            <input type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
+            <input
+                type="text"
+                name='title'
+                value={state.title}
+                onChange={changeHandler}
             />
             <label>Reps:</label>
-            <input type="text"
-                value={reps}
-                onChange={(e) => setReps(e.target.value)}
+            <input
+                type="text"
+                name='reps'
+                value={state.reps}
+                onChange={changeHandler}
             />
             <button type="submit">Add Habit</button>
-            {error && <div className="error">{error}</div>}
-            {success && <div className="success">{success}</div>}
+            {state.error && <div className="error">{state.error}</div>}
+            {state.success && <div className="success">{state.success}</div>}
         </form>
     );
 }
